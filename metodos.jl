035a6f6fe85e39306fa3ea7bfb39e1b3ba2ca089
leticiaoliv <43250283+leticiaoliv@users.noreply.github.com>
2018-11-10 15:44:33 -0200

    Update metodos.jl

--- metodos.jl
+++ metodos.jl
@@ -10,17 +10,63 @@ function euler(f, t₀, y₀, tₙ; n = 100)
 end
 
 function euler_aperfeicoado(f, t₀, y₀, tₙ; n = 100)
-  # Implemente
+  h = (tₙ - t₀) / n
+  t = linspace(t₀, tₙ, n + 1)
+  y = zeros(n + 1)
+  α = β = 0.5
+  δ = γ = 1.0
+  y[1] = y₀
+  for k = 1:n
+    k₁ = f(t[k], y[k])
+    k₂ = f(t[k] + δ * h, y[k] + γ * h * k₁)
+    y[k + 1] = y[k] + h * (α * k₁ + β * k₂)
+  end
+  return t, y
 end
 
 function heun(f, t₀, y₀, tₙ; n = 100)
-  # Implemente
+  h = (tₙ - t₀) / n
+  t = linspace(t₀, tₙ, n + 1)
+  y = zeros(n + 1)
+  α = 0.25
+  β = 0.75
+  δ = γ = 2.0/3.0
+  y[1] = y₀
+  for k = 1:n
+    k₁ = f(t[k], y[k])
+    k₂ = f(t[k] + δ * h, y[k] + γ * h * k₁)
+    y[k + 1] = y[k] + h * (α * k₁ + β * k₂)
+  end
+  return t, y
 end
 
 function midpoint(f, t₀, y₀, tₙ; n = 100)
-  # Implemente
+  h = (tₙ - t₀) / n
+  t = linspace(t₀, tₙ, n + 1)
+  y = zeros(n + 1)
+  α = 0.0
+  β = 1.0
+  δ = γ = 0.5
+  y[1] = y₀
+  for k = 1:n
+    k₁ = f(t[k], y[k])
+    k₂ = f(t[k] + δ * h, y[k] + γ * h * k₁)
+    y[k + 1] = y[k] + h * (α * k₁ + β * k₂)
+  end
+  return t, y
 end
 
 function rungekutta4(f, t₀, y₀, tₙ; n = 100)
-  # Implemente
+  h = (tₙ - t₀) / n
+  t = linspace(t₀, tₙ, n + 1)
+  y = zeros(n + 1)
+  y[1] = y₀
+  for k = 1:n
+    k₁ = f(t[k], y[k])
+    k₂ = f(t[k] + 0.5 * h, y[k] + 0.5 * h * k₁)
+    k₃ = f(t[k] + 0.5 * h, y[k] + 0.5 * h * k₂)
+    k₄ = f(t[k] + h, y[k] + h * k₃)
+    y[k + 1] = y[k] + h * (k₁ + 2.0 * k₂ + 2.0 * k₃ + k₄) / 6.0
+  end
+  return t, y
 end
